147884927c8a467c87ebacad1c6813f73e0d6980
Robert Garcia <robertgarciadsilva@gmail.com>
2020-03-30 14:57:32 -0300

    Uma e duas dimensões para o problema

--- algunsProblemas/randomWalk/main.py
+++ algunsProblemas/randomWalk/main.py
@@ -1,2 +1,27 @@
 
-import 
+import random
+
+def one_dimension_random_walk(numSteps):
+    walk = []
+    posicaoEsperada = 0
+    steps = [-1,1]
+    for i in range(0, numSteps):
+        walk.append(random.choice(steps))
+        posicaoEsperada += walk[i]
+    return walk, posicaoEsperada
+
+def two_dimension_random_walk(numSteps):
+    x_dimension = []; x_esperado = 0
+    y_dimension = []; y_esperado = 0
+    steps = [-1,1]
+    for i in range(0, numSteps):
+        x_dimension.append(random.choice(steps))
+        y_dimension.append(random.choice(steps))
+        x_esperado += x_dimension[i]
+        y_esperado += y_dimension[i]
+    return x_esperado, y_esperado
+
+
+
+x, y = two_dimension_random_walk(100)
+print('x: ', x, 'y: ', y)
